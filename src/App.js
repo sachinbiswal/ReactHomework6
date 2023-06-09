ed6a9e94a75abe473573bacb4c3e5bf11652cdb6
sachin kumar biswal <sachinkbiswal@gmail.com>
2023-06-09 13:21:38 +0530

    Home Work 6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import Navbar from './Component/Navbar';
+import Main from './Component/Main';
+import Footer from './Component/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (<div style = {{ marginLeft : "10%",width : "1000px",heigth : "100%",backgroundColor : "#c8f4d0",boxShadow : "0 0 20px black"}}>
+    <div style = {{margin : "50px",padding : "40px",backgroundColor : "black",borderRadius : "20px"}}>
+    <div style = {{width : "300px",heigth : "60px",width  :"880px",margin : '-30px',padding  :"-30px",backgroundColor:"white"}}>
+      <Navbar />
+      <Main />
+      <Footer />
+
+      </div>
+    </div>
     </div>
   );
 }
